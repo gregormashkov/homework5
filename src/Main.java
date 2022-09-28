a96c0421ab284d03b1e2ed298c015f144e10cb86
grifo <grifoncheg1996@gmail.com>
2022-09-28 12:34:30 +0300

    Домашнее задание по условным операторам

---
+++ src/Main.java
@@ -0,0 +1,181 @@
+public class Main {
+    public static void main(String[] args) {
+        //ДЗ 5, Задача 1
+        int age1 = 19;
+        if (age1 >= 18) {
+            System.out.println("Поздравляю с совершеннолетием!");
+        }
+        int age2 = 17;
+        if (age2 < 18) {
+            System.out.println("Ещё рано, мы тебе пиво не продадим");
+            // Задача 2
+            int child = 7;
+            if (child == 7) {
+                System.out.println("Пора в первый раз в первый класс!");
+            }
+            int youth = 18;
+            if (youth == 18) {
+                System.out.println("Пора в университет");
+            }
+            int unemployed = 24;
+            if (unemployed == 24) {
+                System.out.println("Ищи работу");
+            }
+            // Задача 3
+            int wagon = 102;
+            if (wagon < 102) {
+                System.out.println("Есть свободные места, выберите сидячее или стоячее место");
+            }
+            if (wagon == 102) {
+                System.out.println("К сожалению, свободных мест в вагоне нет");
+            }
+            int sitwagon = 60;
+            if (sitwagon == 60) {
+                System.out.println("Вы можете выбрать только стоячие места");
+            }
+            if (sitwagon < 60) {
+                System.out.println("Выберите свободные сидячие места");
+            }
+            // Переделанные задачи с оператором else
+            // Задача 1
+            int newAge1 = 20;
+            if (newAge1 >= 18) {
+                System.out.println("поздравить с соверщеннолетием");
+            } else {
+                System.out.println("Рано праздновать");
+            }
+            // Задача 2
+            int child1 = 7;
+            if (child1 >= 7) {
+                System.out.println("Пора в школу");
+            } else {
+                System.out.println("Ещё равно");
+            }
+            int youth1 = 18;
+            if (youth1 >= 18) {
+                System.out.println("Поступай в университет");
+            } else {
+                System.out.println("Лучше бы ты об экзаменах думал");
+            }
+            int unemployed1 = 24;
+            if (unemployed1 >= 24) {
+                System.out.println("Ищи работу быстрее");
+            } else {
+                System.out.println("Можно не так быстро, но лучше бы ты уже работал");
+            }
+            // Задача 3
+            int wagon1 = 102;
+            if (wagon1 == 102) {
+                System.out.println("К сожалению, свободных мест нет");
+            } else {
+                System.out.println("Выберите свободные места");
+            }
+            int sitwagon1 = 60;
+            if (sitwagon1 == 60) {
+                System.out.println("Вы можете выбрать только стоячие места");
+            } else {
+                System.out.println("Выберите свободные места");
+            }
+            // Операции сравнения
+            // Задача 1
+            int ageA = 4;
+            if (ageA >= 2 && ageA <= 6) {
+                System.out.println("Если возраст равен 4 годам, то человеку нужно ходить в детский сад");
+            }
+            int ageB = 11;
+            if (ageB >= 7 && ageB <= 18) {
+                System.out.println("Если человеку 11 лет, то ему нужно ходить в школу");
+            }
+            int ageC = 20;
+            if (ageC >= 18 && ageC <= 24) {
+                System.out.println("Если человеку 20 лет, то ему нужно ходить в университет");
+            }
+            int ageD = 26;
+            if (ageD >= 24) {
+                System.out.println("Если человеку 26 лет, то ему пора идти на работу");
+            }
+            // Задача 2
+            int kid1 = 3;
+            if (kid1 < 5) {
+                System.out.println("Дети до 5 лет не допускаются на аттракционы");
+            }
+            int kid2 = 10;
+            if (kid2 >= 5 && kid2 < 14) {
+                System.out.println("Дети от 5 до 14 лет допускаются на аттракционы только в сопровождении взрослых");
+            } else {
+                System.out.println("Без взрослых до 14 лет дети не допускаются на аттракционы");
+            }
+            int kid3 = 16;
+            if (kid3 >= 14 && kid3 < 18) {
+                System.out.println("Детям от 14 до 17 лет можно кататься на аттракционах без сопровождения взрослых");
+            }
+            // Задача 3
+            int one = 1;
+            int two = 2;
+            int three = 3;
+           boolean oneLessTwo = true;
+           if (oneLessTwo) {
+               System.out.println(" 1 < 2");
+           } else {
+               System.out.println(" 1 не может быть больше 2");
+           }
+           boolean oneLessThree = true;
+           if (oneLessThree) {
+               System.out.println(" 1 < 3");
+           } else {
+               System.out.println(" 1 не может быть больше 3");
+           }
+           boolean twoLessThree = true;
+           if (twoLessThree) {
+               System.out.println(" 2 < 3");
+           } else {
+               System.out.println(" 2 не может быть больше 3");
+           }
+           boolean twoMoreOne = true;
+           if (twoMoreOne) {
+               System.out.println(" 2 > 1");
+           } else {
+               System.out.println(" 2 не может быть меньше 1");
+           }
+           boolean threeMoreOne = true;
+           if (threeMoreOne) {
+               System.out.println(" 3 > 1");
+           } else {
+               System.out.println(" 3 не может быть меньше 1");
+           }
+           boolean threeMoreTwo = true;
+           if (threeMoreTwo) {
+               System.out.println(" 3 > 2");
+           } else {
+               System.out.println(" 3 не может быть меньше 1");
+           }
+
+
+
+
+
+
+
+
+
+            }
+
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+            }
+
+
+
+
